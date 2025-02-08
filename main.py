276685281632b39180b60b0f22f242e9bfddf8a3
John Sturgeon <john@sturgeon.me>
2025-02-08 10:38:51 -0800

    Auth flow is complete
    I've added a 'depends' for redirecting to authentication if the auth_token does not exist
    I've also added code to save the auth_token in the browser cookies (expires after 30 days)
    
    This resolves #52 #58 #60 #50

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
 import os
 import urllib.parse
+from typing import Optional, Final
 
 import httpx
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, Depends
+from pydantic import BaseModel
+from starlette import status
 from starlette.responses import JSONResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
+# Constants used for Plex API configuration and cookie settings.
 PLEX_CLIENT_ID = "dc0537e0-6755-44de-97ee-6edd7a51c9b4"  # Generate a UUID for this
 PLEX_REDIRECT_URI = "http://localhost:6701/auth/callback"
 PLEX_PRODUCT_NAME = "Gosh Darned Plex Tools"
 PLEX_PIN_URL = "https://plex.tv/api/v2/pins"
 PLEX_AUTH_URL = "https://app.plex.tv/auth#"
-PLEX_FORWARD_URL = "http://localhost:6701/"
+PLEX_FORWARD_URL = "http://localhost:6701/callback"
+PLEX_USER_URL = "https://plex.tv/api/v2/user"
+SECONDS: Final[int] = 60 * 60 * 24
+DAYS: Final[int] = 30
+COOKIE_TIME_OUT = DAYS * SECONDS
+
+# Initialize the FastAPI app and add session middleware.
 app = FastAPI()
 # noinspection PyTypeChecker
 app.add_middleware(SessionMiddleware, secret_key="some-random-string")
 
 
+class PlexUser(BaseModel):
+    """
+    Model representing a Plex user.
+
+    Attributes:
+        auth_token (str): Authentication token provided by Plex.
+        name (str): The username of the Plex user.
+    """
+
+    auth_token: str
+    name: str
+
+
 @app.middleware("http")
 async def some_middleware(request: Request, call_next):
+    """
+    Middleware to preserve the session cookie across HTTP responses.
+
+    This middleware intercepts incoming HTTP requests and ensures that if the session cookie is present,
+    it is set as an HTTP-only cookie in the outgoing response.
+
+    Args:
+        request (Request): The incoming HTTP request.
+        call_next (Callable): The next middleware or route handler in the chain.
+
+    Returns:
+        Response: The HTTP response with the session cookie set (if present).
+    """
     response = await call_next(request)
     session = request.cookies.get("session")
     if session:
@@ -30,12 +66,81 @@ async def some_middleware(request: Request, call_next):
     return response
 
 
+# Initialize the Jinja2 templates directory.
 templates = Jinja2Templates(directory="templates")
 
+# A simple in-memory store for user tokens (if needed in the future).
 user_tokens = {}
 
 
+async def verify_plex_user(request: Request) -> PlexUser:
+    """
+    Verify the Plex user from the current session or redirect to the login if not authenticated.
+
+    This function attempts to retrieve the Plex user information using the 'auth_token' stored in the cookies.
+    If the token is valid, it returns a PlexUser object. Otherwise, it raises an HTTPException to trigger a
+    redirection to the login route.
+
+    Args:
+        request (Request): The incoming HTTP request.
+
+    Returns:
+        PlexUser: The authenticated Plex user.
+
+    Raises:
+        HTTPException: If the user is not authenticated, with a temporary redirect header to the login page.
+    """
+    plex_user = None
+    auth_token = request.cookies.get("auth_token")
+    if auth_token:
+        plex_user: Optional[PlexUser] = await get_plex_user_from_auth_token(auth_token)
+    if plex_user:
+        return plex_user
+
+    if request.cookies.get("auth_token"):
+        request.cookies.pop("auth_token")
+    raise HTTPException(
+        status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+        headers={"Location": "/auth/login"},
+    )
+
+
+async def get_auth_token_from_pin(request: Request) -> Optional[str]:
+    """
+    Retrieve the authentication token from a previously generated PIN stored in session.
+
+    This function checks if the session contains PIN information. If present, it uses that information
+    to fetch the authentication token from the Plex API. Once the token is retrieved, it removes the
+    PIN information from the session.
+
+    Args:
+        request (Request): The incoming HTTP request.
+
+    Returns:
+        Optional[str]: The retrieved authentication token if available; otherwise, None.
+    """
+    auth_token: Optional[str] = None
+    pin_info = request.session.get("pin_info")
+    if pin_info:
+        pin_data = await fetch_auth_token(
+            pin_id=pin_info.get("pin_id"), pin_code=pin_info.get("pin_code")
+        )
+        auth_token = pin_data.get("authToken")
+        request.session.pop("pin_info")
+    return auth_token
+
+
 async def generate_pin():
+    """
+    Generate a PIN for Plex authentication.
+
+    This function calls the Plex PIN API to generate a new PIN for user authentication. It sends a POST request
+    with the required headers and returns the JSON response containing the PIN details.
+
+    Returns:
+        dict or JSONResponse: The JSON response with PIN information if successful,
+                              or a JSONResponse with an error message if PIN generation fails.
+    """
     async with httpx.AsyncClient() as client:
         response = await client.post(
             PLEX_PIN_URL,
@@ -56,6 +161,20 @@ async def generate_pin():
 
 
 async def fetch_auth_token(pin_id, pin_code):
+    """
+    Fetch the authentication token using a given PIN ID and code.
+
+    This function queries the Plex PIN API with the provided PIN ID and PIN code to retrieve
+    the associated authentication token.
+
+    Args:
+        pin_id (str): The ID of the generated PIN.
+        pin_code (str): The code corresponding to the PIN.
+
+    Returns:
+        dict or JSONResponse: The JSON response containing the authentication token if successful,
+                              or a JSONResponse with an error message if the token cannot be fetched.
+    """
     url = f"{PLEX_PIN_URL}/{pin_id}"
     async with httpx.AsyncClient() as client:
         response = await client.get(
@@ -73,27 +192,108 @@ async def fetch_auth_token(pin_id, pin_code):
     return response_json
 
 
+async def get_plex_user_from_auth_token(auth_token) -> Optional[PlexUser]:
+    """
+    Retrieve Plex user information using an authentication token.
+
+    This function sends a GET request to the Plex user API endpoint with the provided authentication token.
+    If the response is successful, it returns a PlexUser object containing the user's details; otherwise, it returns None.
+
+    Args:
+        auth_token (str): The authentication token for the Plex user.
+
+    Returns:
+        Optional[PlexUser]: A PlexUser object if successful; otherwise, None.
+    """
+    url = f"{PLEX_USER_URL}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url,
+            headers={
+                "accept": "application/json",
+                "X-Plex-Product": PLEX_PRODUCT_NAME,
+                "X-Plex-Client-Identifier": PLEX_CLIENT_ID,
+                "X-Plex-Token": auth_token,
+            },
+        )
+
+    if response.status_code != 200:
+        return None
+    response_json = response.json()
+    user = PlexUser(name=response_json["username"], auth_token=auth_token)
+    return user
+
+
 @app.get("/")
-async def root(request: Request):
-    pin_info = request.session.get("pin_info")
-    pin_data = await fetch_auth_token(
-        pin_id=pin_info.get("pin_id"), pin_code=pin_info.get("pin_code")
-    )
-    pin_info["auth_token"] = pin_data.get("authToken")
+async def root(request: Request, plex_user: PlexUser = Depends(verify_plex_user)):
+    """
+    Render the home page for authenticated Plex users.
+
+    This route handler renders the home page template with the authenticated user's information.
+
+    Args:
+        request (Request): The incoming HTTP request.
+        plex_user (PlexUser): The authenticated Plex user, obtained via dependency injection.
+
+    Returns:
+        TemplateResponse: The rendered home page.
+    """
     return templates.TemplateResponse(
         "home.j2",
         {
             "request": request,
-            "pin_id": pin_info.get("pin_id"),
-            "auth_token": pin_info.get("auth_token"),
+            "name": plex_user.name,
+            "auth_token": plex_user.auth_token,
         },
     )
+
+
+@app.get("/callback")
+async def callback(request: Request):
+    """
+    Handle the callback from Plex authentication.
+
+    After Plex authentication, this route is called to retrieve the authentication token using the PIN
+    information stored in the session. Depending on whether the token retrieval is successful, the user is
+    redirected either to the home page or to the login page.
+
+    Args:
+        request (Request): The incoming HTTP request containing session data.
+
+    Returns:
+        RedirectResponse: A redirection to the home page if authentication is successful,
+                          or to the login page if not.
+    """
+    auth_token = await get_auth_token_from_pin(request)
+    if auth_token:
+        redirect_url = request.url_for("root")
+        response = RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
+        response.set_cookie(
+            key="auth_token",
+            value=auth_token,
+            max_age=COOKIE_TIME_OUT,
+        )
+    else:
+        redirect_url = request.url_for("login")
+        response = RedirectResponse(redirect_url, status_code=status.HTTP_302_FOUND)
+    return response
 
 
 @app.get("/auth/login")
 async def login(request: Request):
     """
-    Step 1: Generate a PIN and redirect the user to Plex for authentication.
+    Initiate the Plex login process by generating a PIN and redirecting to Plex's authentication page.
+
+    This route handler calls the Plex PIN API to generate a new PIN, builds the authentication URL using the
+    generated PIN, and stores the PIN information in the session. The user is then redirected to Plex to complete
+    the authentication process.
+
+    Args:
+        request (Request): The incoming HTTP request.
+
+    Returns:
+        RedirectResponse: A redirection to the Plex authentication URL, or a JSONResponse with an error message
+                          if the PIN generation fails.
     """
     request_pin_info = await generate_pin()
     pin_code = request_pin_info.get("code")
@@ -109,7 +309,7 @@ async def login(request: Request):
         + "&forwardUrl="
         + urllib.parse.quote(PLEX_FORWARD_URL)
     )
-    # Store the pin_id temporarily
+    # Store the PIN information temporarily in the session.
     request.session["pin_info"] = {
         "pin_id": pin_id,
         "pin_code": pin_code,
@@ -120,5 +320,12 @@ async def login(request: Request):
 
 
 if __name__ == "__main__":
+    """
+    Entry point for running the application.
+
+    This block checks the 'ENVIRONMENT' variable to determine whether to enable auto-reloading.
+    It then runs the FastAPI application using uvicorn on host '0.0.0.0' and port 6701.
+    """
     reload: bool = os.getenv("ENVIRONMENT") != "production"
+    print("Log in at: http://localhost:6701/")
     uvicorn.run("main:app", host="0.0.0.0", port=6701, reload=reload, access_log=False)
